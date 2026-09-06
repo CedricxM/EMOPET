8ba009cecc61fa779754bf53d9c4274a5446a2d9
Cedric Mian <160498027+CedricxM@users.noreply.github.com>
2026-09-06 22:22:43 +0200

    feat: export professional share validators

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,7 @@
 export * from './types/index.js';
 export * from './constants/index.js';
 export * from './validators/index.js';
+export * from './validators/professional-share.js';
 export * from './config/index.js';
 export * from './sensors/registry.js';
 export * from './entitlements/index.js';
